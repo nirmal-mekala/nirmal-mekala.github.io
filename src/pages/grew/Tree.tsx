c1bb06e95d5299b720883b4e0f9f63ab2cff2dd1
Nirmal Mekala <ndmekala@icloud.com>
2025-08-19 21:16:37 -0500

    add react + tailwind. stub out a tree component

---
+++ src/pages/grew/Tree.tsx
@@ -0,0 +1,14 @@
+export const Tree = () => {
+  return (
+    <div className="px-8 border border-fuchsia-500">
+      hi
+      <svg width="250" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon
+          points="5,0 15,0 20,100 0,100"
+          fill="var(--fg-color-2)"
+          stroke="var(--fg-color-2)"
+        />
+      </svg>
+    </div>
+  );
+};
