d5a163f82ff8192d7d50cedfb72c9c2c844dc4f6
Nirmal Mekala <ndmekala@icloud.com>
2025-08-25 11:02:00 -0500

    growing treeee

--- src/pages/grew/Tree.tsx
+++ src/pages/grew/Tree.tsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 type Point = { x: number; y: number };
 
 const convertY = (y: number, height: number): number => {
@@ -16,52 +19,28 @@ const Line = (props: {
       x2={point2.x}
       y1={convertY(point1.y, containerHeight)}
       y2={convertY(point2.y, containerHeight)}
-      style={{ stroke: "var(--fg-color-2)", strokeWidth: 2 }}
+      style={{ stroke: "var(--fg-color-1)", strokeWidth: 2 }}
     ></line>
   );
 };
 
-type TreeConfig = {
+type BranchConfig = {
   depth: number;
-  branchCount: number;
+  size: number;
+  rawDepth: number;
+  // branchCount: number;
   //  trunkHeight: number;
   //  trunkWidth: number;
   //  angleSpread: number;
 };
 
-/*
-
-let point1 = { x: 0, y: 0 };
-let fortyFiveDegreePpoint = {x: 1, y: 1};
-let sixtyDegreePoint = {x: 1, y: Math.sqrt(3)};
-
-// x2 + y2 = (2x)2
-// x^2 + y^2 = 4 * x^2
-// y^2 = 3 * x^2
-// y = sqrt(3 * x^2)
-
-
-
-let fourtyFiveDegreeAngle = Math.atan2(fortyFiveDegreePpoint.y - point1.y, fortyFiveDegreePpoint.x - point1.x);
-let sixtyDegreeAngle = Math.atan2(sixtyDegreePoint.y - point1.y, sixtyDegreePoint.x - point1.x);
-console.log(fourtyFiveDegreeAngle/Math.PI, sixtyDegreeAngle/Math.PI);
-
-angle = (point) => {
-  return Math.atan2(point.y - point1.y, point.x - point1.x)/Math.PI;
-}
-
-let fourThirty = {x: 1, y: -1};
-let sevenThirty = {x: -1, y: -1};
-let tenThirty = {x: -1, y: 1};
-
-console.log(angle(fourThirty), angle(sevenThirty), angle(tenThirty));
-
-*/
-
-const getTreeConfig = (size: number): TreeConfig => {
+const getBranchConfig = (size: number): BranchConfig => {
+  const rawDepth = size / 20;
   return {
-    depth: Math.floor(size / 20),
-    branchCount: Math.min(5, Math.floor(size / 30)),
+    depth: Math.floor(rawDepth),
+    rawDepth,
+    size: size,
+    // branchCount: Math.min(5, Math.floor(size / 30)),
     //    trunkHeight: size * 0.4,
     //    trunkWidth: size * 0.1,
     //    angleSpread: 30, // degrees of total angle to spread child branches
@@ -77,12 +56,29 @@ const angle = (point1: Point, point2: Point): number => {
   return Math.atan2(point2.y - point1.y, point2.x - point1.x);
 };
 
-const nextPointsTrig = (point1: Point, point2: Point): Array<Point> => {
+const nextPoints = (
+  point1: Point,
+  point2: Point,
+  // TODO unrelated but astro-ish refactor of redirect
+  // TODO - consider sending the obj again, this is verbose...
+  depth: BranchConfig["depth"],
+  size: BranchConfig["size"],
+  rawDepth: BranchConfig["rawDepth"],
+  currentDepth: number,
+): Array<Point> => {
   const angleVal = angle(point1, point2);
-  const angleSpread = Math.PI / 6; // 30 degrees
+  const angleSpread = Math.PI / 6;
   const length = getLength(point1, point2);
-  const lengthDivisor = 1.75;
-  const newLength = length / lengthDivisor;
+  // TODO this is very grug; refactor
+  const progressToNextDepth = rawDepth - depth;
+  const lengthMultiplier = () => {
+    if (currentDepth === depth - 2) {
+      return 0.5 * progressToNextDepth;
+    }
+    return 0.5;
+  };
+  const newLength = length * lengthMultiplier();
+
   const result = [angleVal - angleSpread, angleVal, angleVal + angleSpread].map(
     (angle) => {
       const xDiff = newLength * Math.cos(angle);
@@ -96,66 +92,28 @@ const nextPointsTrig = (point1: Point, point2: Point): Array<Point> => {
   return result;
 };
 
-const nextPointsAlg = (point1: Point, point2: Point): Array<Point> => {
-  const getDirection = (
-    point1: Point,
-    point2: Point,
-  ): "n" | "e" | "s" | "w" => {
-    if (point1.x > point2.x) {
-      return "w";
-    }
-    if (point1.x < point2.x) {
-      return "e";
-    }
-    if (point1.y > point2.y) {
-      return "s";
-    }
-    return "n";
-  };
-
-  const direction = getDirection(point1, point2);
-  const length = getLength(point1, point2);
-  const lengthDivisor = 2;
-  switch (direction) {
-    case "n":
-      return [
-        { x: point2.x - length / lengthDivisor, y: point2.y },
-        { x: point2.x + length / lengthDivisor, y: point2.y },
-        { x: point2.x, y: point2.y + length / lengthDivisor },
-      ];
-    case "s":
-      return [
-        { x: point2.x - length / lengthDivisor, y: point2.y },
-        { x: point2.x + length / lengthDivisor, y: point2.y },
-        { x: point2.x, y: point2.y - length / lengthDivisor },
-      ];
-    case "e":
-      return [
-        { x: point2.x, y: point2.y - length / lengthDivisor },
-        { x: point2.x, y: point2.y + length / lengthDivisor },
-        { x: point2.x + length / lengthDivisor, y: point2.y },
-      ];
-    case "w":
-      return [
-        { x: point2.x, y: point2.y - length / lengthDivisor },
-        { x: point2.x, y: point2.y + length / lengthDivisor },
-        { x: point2.x - length / lengthDivisor, y: point2.y },
-      ];
-  }
-};
-
 const Branch = (props: {
-  depth: number;
+  currentDepth: number;
   branchNumber: number;
   point1: Point;
   point2: Point;
-  config: TreeConfig;
+  depth: BranchConfig["depth"];
+  size: BranchConfig["size"];
+  rawDepth: BranchConfig["rawDepth"];
   containerHeight: number;
 }) => {
-  const { depth, branchNumber, point1, point2, config, containerHeight } =
-    props;
+  const {
+    currentDepth,
+    branchNumber,
+    point1,
+    point2,
+    depth,
+    size,
+    rawDepth,
+    containerHeight,
+  } = props;
 
-  if (depth >= config.depth) {
+  if (currentDepth >= depth) {
     const points = [
       [point1.x, point1.y],
       [point1.x, point1.y + 5],
@@ -164,22 +122,35 @@ const Branch = (props: {
     ]
       .map((v) => [v[0], convertY(v[1], containerHeight)].join(","))
       .join(" ");
-    return <polygon points={points} fill="blue" stroke="blue" />;
+    return (
+      <polygon
+        points={points}
+        fill="var(--fg-color-2)"
+        stroke="var(--fg-color-2)"
+      />
+    );
   }
-  const nextPoints = nextPointsTrig;
+
+  const np = nextPoints(point1, point2, depth, size, rawDepth, currentDepth);
 
   return (
     <>
-      <Line point1={point1} point2={point2} containerHeight={containerHeight} />
-      {nextPoints(point1, point2).map((nextPoint, index) => {
+      <Line
+        point1={{ ...point1 }}
+        point2={{ ...point2 }}
+        containerHeight={containerHeight}
+      />
+      {np.map((nextPoint, index) => {
         return (
           <Branch
-            key={`${depth}-${branchNumber}-${index}`}
+            key={`${currentDepth}-${branchNumber}-${index}`}
             point1={point2}
             point2={nextPoint}
-            depth={depth + 1}
+            currentDepth={currentDepth + 1}
             branchNumber={index}
-            config={config}
+            depth={depth}
+            size={size}
+            rawDepth={rawDepth}
             containerHeight={containerHeight}
           />
         );
@@ -191,16 +162,39 @@ const Branch = (props: {
 // TODO break components out into own file
 
 export const Tree = () => {
+  const [size, setSize] = useState(100);
+  const [config, setConfig] = useState(getBranchConfig(size));
   const containerHeight = 500;
   const width = 500;
-  const size = 100;
+  //  const size = 100;
+  //
+  useEffect(() => {
+    const v = getBranchConfig(size);
+    setConfig(Object.assign({}, v));
+  }, [size]);
 
-  const config = getTreeConfig(size);
+  //  const config = getBranchConfig(size);
 
   // TODO --> path?
+  const handleSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSize(Number(e.target.value));
+  };
 
   return (
     <div className="px-8 border border-fuchsia-500">
+      <label>
+        size
+        <input
+          style={{ marginLeft: "10px" }}
+          id="size"
+          type="number"
+          min={20}
+          max={145}
+          value={size}
+          onChange={handleSizeChange}
+        />
+      </label>
+      <p>{size}</p>
       <svg
         width={width}
         height={containerHeight}
@@ -208,15 +202,46 @@ export const Tree = () => {
         style={{ backgroundColor: "var(--bg-color-2)" }}
       >
         <Branch
-          depth={0}
-          key={`0`}
+          currentDepth={0}
+          key={uuidv4()}
           branchNumber={0}
           point1={{ x: width / 2, y: 0 }}
-          point2={{ x: width / 2, y: containerHeight / 3 }}
-          config={config}
+          point2={{ x: width / 2, y: (size * containerHeight) / 300 }}
+          depth={config.depth}
+          size={config.size}
+          rawDepth={config.rawDepth}
+          containerHeight={containerHeight}
+        />
+        <Branch
+          currentDepth={0}
+          key={uuidv4()}
+          branchNumber={1}
+          point1={{ x: width / 2, y: 0 }}
+          point2={{
+            x: width / 2 + (size * containerHeight) / 600,
+            y: (size * containerHeight) / 600,
+          }}
+          depth={config.depth}
+          size={config.size}
+          rawDepth={config.rawDepth}
+          containerHeight={containerHeight}
+        />
+        <Branch
+          currentDepth={0}
+          key={uuidv4()}
+          branchNumber={1}
+          point1={{ x: width / 2, y: 0 }}
+          point2={{
+            x: width / 2 - (size * containerHeight) / 600,
+            y: (size * containerHeight) / 600,
+          }}
+          depth={config.depth}
+          size={config.size}
+          rawDepth={config.rawDepth}
           containerHeight={containerHeight}
         />
       </svg>
     </div>
   );
+  // TODO ^^ containerHeight function of size
 };
