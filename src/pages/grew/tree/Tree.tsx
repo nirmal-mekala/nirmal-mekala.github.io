beefbc1cead81ffe4eea4211a97b6205fd473600
Nirmal Mekala <ndmekala@icloud.com>
2025-10-17 08:55:00 -0500

    small changes

--- src/pages/grew/tree/Tree.tsx
+++ src/pages/grew/tree/Tree.tsx
@@ -11,6 +11,7 @@ export const Tree = () => {
   const TREE_FINAL_SIZE = 119;
   const TREE_SVG_SIZE = 600;
   const TREE_SVG_CENTER_X = TREE_SVG_SIZE / 2;
+
   const basePoint = { x: TREE_SVG_CENTER_X, y: 0 };
   const [treeSize, setTreeSize] = useState<number>(TREE_STARTING_SIZE);
   const treeSizeRef = useRef<number>(TREE_STARTING_SIZE);
@@ -20,17 +21,27 @@ export const Tree = () => {
     return (size * TREE_SVG_SIZE * 2) / TREE_SVG_SIZE;
   };
 
-  // TODO --> improve typing around setters from useState fns
-
-  const scrollRatioToTreeSize = (ratio: number): number => {
+  const scrollProgressToTreeSize = (ratio: number): number => {
+    /*
+     * Convert a scroll progress float to a tree size float
+     * Rounding here is crucial to conserve renders
+     * (tree size doesnt change for small progress changes)
+     */
     const rawSize =
       (TREE_FINAL_SIZE - TREE_STARTING_SIZE) * ratio + TREE_STARTING_SIZE;
     const size = Math.ceil(3 * rawSize) / 3;
     return size;
   };
 
-  // TODO as part of de-linearizing, ideally we would have effective
-  // scroll ratio as the state variable / ref and then we maybe
+  const treeSizeToScrollProgress = (size: number): number => {
+    /*
+     * Infer our progress from the current tree size
+     */
+    const treeSizeRange = TREE_FINAL_SIZE - TREE_STARTING_SIZE;
+    const currentSizeDelta = size - TREE_STARTING_SIZE;
+    return currentSizeDelta / treeSizeRange;
+  };
+
   useEffect(() => {
     treeSizeRef.current = treeSize;
   }, [treeSize]);
@@ -42,17 +53,17 @@ export const Tree = () => {
     scrollableHeight.current = scrollable;
   }, []);
 
-  const getScrollRatio = (): number => {
+  const getScrollProgress = (): number => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
     const sh = scrollableHeight.current;
     if (!sh) return 0;
-    const scrollRatio = scrollTop / sh;
-    return scrollRatio;
+    const scrollProgress = Math.min(1, scrollTop / sh);
+    return scrollProgress;
   };
 
   const handleScroll = () => {
-    const scrollRatio = getScrollRatio();
-    const calculatedTreeSize = scrollRatioToTreeSize(scrollRatio);
+    const scrollProgress = getScrollProgress();
+    const calculatedTreeSize = scrollProgressToTreeSize(scrollProgress);
     if (calculatedTreeSize !== treeSizeRef.current) {
       setTreeSize(calculatedTreeSize);
     }
@@ -65,8 +76,28 @@ export const Tree = () => {
     };
   }, []);
 
+  // TODO MOVE ALL OF THIS TO IS OWN COMP
+  // =====================================
+
+  const messages: Array<{ msg: string; progressTrigger: number }> = [
+    {
+      msg: "hello",
+      progressTrigger: 0.15,
+    },
+    {
+      msg: "message 2",
+      progressTrigger: 0.5,
+    },
+    {
+      msg: "message 3",
+      progressTrigger: 0.75,
+    },
+  ];
+
+  // =====================================
+
   return (
-    <div className="px-8 h-[800vh] relative flex justify-center">
+    <div className="px-8 h-[800vh] relative flex justify-center flex-nowrap">
       <svg
         viewBox={`0 0 ${TREE_SVG_SIZE} ${TREE_SVG_SIZE}`}
         preserveAspectRatio="xMinYMin meet"
@@ -75,7 +106,7 @@ export const Tree = () => {
           backgroundColor: "transparent",
           width: `min(${TREE_SVG_SIZE}px, 100%)`,
         }}
-        className="rotate-180 fixed top-0"
+        className="rotate-180 fixed top-0 border border-fuchsia-500"
       >
         <Branch
           currentDepth={0}
@@ -89,6 +120,17 @@ export const Tree = () => {
           parentPoints={[basePoint, basePoint]}
         />
       </svg>
+      <div className={`fixed w-[600px]  top-[600px]`}>
+        <ol>
+          {messages.map(({ msg, progressTrigger }) => {
+            return (
+              treeSizeToScrollProgress(treeSize) > progressTrigger && (
+                <li>{msg}</li>
+              )
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
 };
