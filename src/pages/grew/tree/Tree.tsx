53305a74e1084fa9a58953a28c5547db6fcf071a
Nirmal Mekala <ndmekala@icloud.com>
2025-10-21 00:50:00 -0500

    build out some copy

--- src/pages/grew/tree/Tree.tsx
+++ src/pages/grew/tree/Tree.tsx
@@ -1,56 +1,38 @@
-import { useState, useEffect, useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { Branch } from "./Branch.tsx";
+import { useEffect, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Branch } from './Branch.tsx';
+import { Copy } from './Copy.tsx';
 
-// TODO stagger it a bit more. relationship between scroll and size
-// maybe should not be linear, should be on some curve
+export const TREE_STARTING_SIZE = 41;
+export const TREE_FINAL_SIZE = 119;
+export const TREE_SVG_SIZE = 500;
+export const TREE_SVG_CENTER_X = TREE_SVG_SIZE / 2;
 
 export const Tree = () => {
-  // TODO consider screaming snake case...
-  const TREE_STARTING_SIZE = 41;
-  const TREE_FINAL_SIZE = 119;
-  const TREE_SVG_SIZE = 600;
-  const TREE_SVG_CENTER_X = TREE_SVG_SIZE / 2;
-
   const basePoint = { x: TREE_SVG_CENTER_X, y: 0 };
   const [treeSize, setTreeSize] = useState<number>(TREE_STARTING_SIZE);
   const treeSizeRef = useRef<number>(TREE_STARTING_SIZE);
   const scrollableHeight = useRef<number | null>(null);
-
-  const treeHeight = (size: number) => {
-    return (size * TREE_SVG_SIZE * 2) / TREE_SVG_SIZE;
-  };
-
-  const scrollProgressToTreeSize = (ratio: number): number => {
-    /*
-     * Convert a scroll progress float to a tree size float
-     * Rounding here is crucial to conserve renders
-     * (tree size doesnt change for small progress changes)
-     */
-    const rawSize =
-      (TREE_FINAL_SIZE - TREE_STARTING_SIZE) * ratio + TREE_STARTING_SIZE;
-    const size = Math.ceil(3 * rawSize) / 3;
-    return size;
-  };
-
-  const treeSizeToScrollProgress = (size: number): number => {
-    /*
-     * Infer our progress from the current tree size
-     */
-    const treeSizeRange = TREE_FINAL_SIZE - TREE_STARTING_SIZE;
-    const currentSizeDelta = size - TREE_STARTING_SIZE;
-    return currentSizeDelta / treeSizeRange;
-  };
 
   useEffect(() => {
     treeSizeRef.current = treeSize;
   }, [treeSize]);
 
   useEffect(() => {
-    const docHeight = document.documentElement.scrollHeight;
-    const winHeight = window.innerHeight;
-    const scrollable = docHeight - winHeight;
-    scrollableHeight.current = scrollable;
+    const calculateScrollableHeight = () => {
+      const docHeight = document.documentElement.scrollHeight;
+      const winHeight = window.innerHeight;
+      const scrollable = docHeight - winHeight;
+      scrollableHeight.current = scrollable;
+    };
+
+    calculateScrollableHeight();
+
+    window.addEventListener('resize', calculateScrollableHeight);
+
+    return () => {
+      window.removeEventListener('resize', calculateScrollableHeight);
+    };
   }, []);
 
   const getScrollProgress = (): number => {
@@ -70,31 +52,11 @@ export const Tree = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
-  // TODO MOVE ALL OF THIS TO IS OWN COMP
-  // =====================================
-
-  const messages: Array<{ msg: string; progressTrigger: number }> = [
-    {
-      msg: "hello",
-      progressTrigger: 0.15,
-    },
-    {
-      msg: "message 2",
-      progressTrigger: 0.5,
-    },
-    {
-      msg: "message 3",
-      progressTrigger: 0.75,
-    },
-  ];
-
-  // =====================================
 
   return (
     <div className="px-8 h-[800vh] relative flex justify-center flex-nowrap">
@@ -103,10 +65,10 @@ export const Tree = () => {
         preserveAspectRatio="xMinYMin meet"
         xmlns="http://www.w3.org/2000/svg"
         style={{
-          backgroundColor: "transparent",
+          backgroundColor: 'transparent',
           width: `min(${TREE_SVG_SIZE}px, 100%)`,
         }}
-        className="rotate-180 fixed top-0 border border-fuchsia-500"
+        className="rotate-180 fixed top-0"
       >
         <Branch
           currentDepth={0}
@@ -115,22 +77,27 @@ export const Tree = () => {
           point1={basePoint}
           point2={{ x: TREE_SVG_CENTER_X, y: treeHeight(treeSize) }}
           treeSize={treeSize}
-          branchOrigin={"trunk"}
-          branchOrientation={"center"}
+          branchOrigin={'trunk'}
+          branchOrientation={'center'}
           parentPoints={[basePoint, basePoint]}
         />
       </svg>
-      <div className={`fixed w-[600px]  top-[600px]`}>
-        <ol>
-          {messages.map(({ msg, progressTrigger }) => {
-            return (
-              treeSizeToScrollProgress(treeSize) > progressTrigger && (
-                <li>{msg}</li>
-              )
-            );
-          })}
-        </ol>
-      </div>
+      <Copy treeSize={treeSize} />
     </div>
   );
+};
+
+const treeHeight = (size: number) => {
+  return (size * TREE_SVG_SIZE * 1.75) / TREE_SVG_SIZE;
+};
+
+const scrollProgressToTreeSize = (ratio: number): number => {
+  /*
+   * Convert a scroll progress float to a tree size float
+   * Rounding here is crucial to conserve renders
+   * (tree size doesnt change for small progress changes)
+   */
+  const rawSize = (TREE_FINAL_SIZE - TREE_STARTING_SIZE) * ratio + TREE_STARTING_SIZE;
+  const size = Math.ceil(3 * rawSize) / 3;
+  return size;
 };
--- src/pages/grew/tree/utils.ts
+++ src/pages/grew/tree/utils.ts
@@ -1,20 +1,14 @@
-import type { Point } from "./types.ts";
+import type { Point } from './types.ts';
 
 export const length = (point1: Point, point2: Point): number => {
-  return Math.sqrt(
-    Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2),
-  );
+  return Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2);
 };
 
 export const angle = (point1: Point, point2: Point): number => {
   return Math.atan2(point2.y - point1.y, point2.x - point1.x);
 };
 
-export const buildPoint = (
-  originalPoint: Point,
-  angle: number,
-  length: number,
-): Point => {
+export const buildPoint = (originalPoint: Point, angle: number, length: number): Point => {
   return {
     x: originalPoint.x + length * Math.cos(angle),
     y: originalPoint.y + length * Math.sin(angle),
@@ -24,7 +18,7 @@ export const buildPoint = (
 export const pointsToSvgPolygonString = (points: Array<Point>) => {
   return points
     .map((p: Point) => {
-      return p.x + "," + p.y;
+      return `${p.x},${p.y}`;
     })
-    .join(" ");
+    .join(' ');
 };
